9a21813eac17d253b781942f28a08be27145ea17
Dilinaraveen <dilinaraveen@gmail.com>
2025-03-23 01:00:12 +0530

    implemented course details page and course details page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, use, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
@@ -31,6 +32,45 @@ export const AppContextProvider = (props) => {
         return Math.floor(totalRating / course.courseRatings.length)
     }
 
+    //Function to calculate course chapter time
+    const calculateChapterTime = (chapter) => {
+
+        let time = 0
+
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
+
+    }
+
+    // Function to Calculate Course Duration
+    const calculateCourseDuration = (course) => {
+
+        let time = 0
+
+        course.courseContent.map(
+            (chapter) => chapter.chapterContent.map(
+                (lecture) => time += lecture.lectureDuration
+            )
+        )
+
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
+
+    }
+
+    //Function to calculate total number of lectures in a course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if (Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length;
+            }
+        });
+        return totalLectures;
+    }
+
+
+
     useEffect(() => {
         fetchAllCourses()
     }, [])
@@ -42,7 +82,10 @@ export const AppContextProvider = (props) => {
         navigate,
         calculateRating,
         isEducator,
-        setIsEducator
+        setIsEducator,
+        calculateChapterTime,
+        calculateCourseDuration,
+        calculateNoOfLectures
     }
 
     return (
