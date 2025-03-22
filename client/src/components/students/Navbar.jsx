97521076192bbf64abb562c99d48e01388204d34
Dilinaraveen <dilinaraveen@gmail.com>
2025-03-22 22:44:27 +0530

    implemented courses section, testimonials section and footer.

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link, useLocation } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext';
 
 function Navbar() {
+
+    const {navigate, isEducator} = useContext(AppContext);
 
     const location = useLocation();
 
     const isCoursesListPage= location.pathname.includes( '/course-list' );
 
     const user = true;
-    const isEducator = false;
 
     return (
         <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCoursesListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-      <img  src={assets.logo} alt="Logo" className="w-28 lg:w-32 cursor-pointer" />
+      <img onClick={()=> navigate('/')}  src={assets.logo} alt="Logo" className="w-28 lg:w-32 cursor-pointer" />
       <div className="md:flex hidden items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
           {
             user && <>
-              <button>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              <button onClick={()=>navigate('/educator')}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
               | <Link to='/my-enrollments' >My Enrollments</Link>
             </>
           }
@@ -32,7 +34,7 @@ function Navbar() {
       {/* For Phone Screens */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
-          <button>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+          <button onClick={()=>navigate('/educator')}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
           | {
             user && <Link to='/my-enrollments' >My Enrollments</Link>
           }
--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../../components/students/Hero'
+import Companies from '../../components/students/Companies'
+import CoursesSection from '../../components/students/CoursesSection'
+import TestimonialsSection from '../../components/students/TestimonialsSection'
+import Footer from '../../components/students/Footer'
 
 function Home() {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
+      <Companies />
+      <CoursesSection />
+      <TestimonialsSection/>
+      <Footer />
     </div>
   )
 }
