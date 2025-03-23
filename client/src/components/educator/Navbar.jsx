a69ec6c991559e37c50b9e636476c794822987a4
Dilinaraveen <dilinaraveen@gmail.com>
2025-03-23 10:56:01 +0530

    implemented educator navbar, sidebar and footer

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { assets, dummyEducatorData } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+
+const Navbar = () => {
+
+ const educatorData = dummyEducatorData
+  const user  = { fullName: "Dilina Raveen" }
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={`flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3`}>
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
+      </Link>
+      <div className="flex items-center gap-5 text-gray-500 relative">
+        <p>Hi! {user.fullName}</p>
+        <div>User Button</div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
