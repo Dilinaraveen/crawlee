a4bfd141da3d9b951cd8e35ad5748945161fe26e
Nethminikavindya <muthuhewaamnk.20@uom.lk>
2025-03-24 13:12:46 +0530

    implemented the verify functions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,7 +44,7 @@ export const register = async (req, res) => {
             text: `Welcome to Crawlee web site.Your account has been created with email id:${email}`
         };
         await transporter.sendMail(mailOptions);
-             return res.json({ success: true });
+        return res.json({ success: true });
 
     } catch (error) {
         res.json({ success: false, message: error.message })
@@ -101,5 +101,64 @@ export const logout = async (req, res) => {
         return res.json({ success: true, message: "Logged Out" });
     } catch (error) {
         return res.json({ success: false, message: error.message })
+    }
+}
+//Send verification OTP to the User's Email
+export const sendVerifyOtp = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await userModel.findById(userId);
+
+        if (user.isAccountVerified) {
+            return res.json({ success: false, message: "Account Already Verified" })
+        }
+        const otp = String(Math.floor(100000 + Math.random() * 900000))
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: `Account Verification OTP`,
+            text: `Your OTP is ${otp}. Verify your account using this OTP.`
+        };
+        await transporter.sendMail(mailOptions);
+        return res.json({ success: true, message: 'Verification OTP Sent on Email' });
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
+export const verifyEmail = async (req, res) => {
+    const { userId, otp } = req.body;
+
+    if (!userId || !otp) {
+        return res.json({ success: false, message: 'Missiing Details' });
+    }
+    try {
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.json({ success: false, message: 'User not found' });
+        }
+        if (user.verifyOtp === '' || user.verifyOtp !== otp) {
+            return res.json({ success: false, message: 'Invalid OTP' });
+        }
+
+        if (user.verifyOtpExpireAt < Date.now()) {
+            return res.json({ success: false, message: 'OTP Expired' });
+        }
+        user.isAccountVerified=true;
+        user.verifyOtp='';
+        user.verifyOtpExpireAt=0;
+        await user.save();
+        return res.json({ success: true, message:'Email verified successfully' });
+
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
     }
 }
