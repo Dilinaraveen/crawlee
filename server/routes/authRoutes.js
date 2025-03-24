62a5570b2917926d55a0e72425e37e4d35344940
Nethminikavindya <muthuhewaamnk.20@uom.lk>
2025-03-24 18:45:04 +0530

    implemented the Reset otp generation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {login,logout,register,sendVerifyOtp,verifyEmail} from '../controllers/authController.js'
+import {login,logout,register,sendVerifyOtp,verifyEmail,isAuthenticated} from '../controllers/authController.js'
 import userAuth from '../config/middleware/userAuth.js';
 
 
@@ -10,6 +10,7 @@ authRouter.post('/login',login);
 authRouter.post('/logout',logout);
 authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp);
 authRouter.post('/verify-account',userAuth,verifyEmail);
+authRouter.post('/is-auth',userAuth,isAuthenticated);
 
 
 export default authRouter;
