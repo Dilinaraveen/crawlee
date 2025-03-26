96bf5c800632ce362777a8f8b777be89093bf142
Nethminikavindya <muthuhewaamnk.20@uom.lk>
2025-03-26 21:10:22 +0530

    implemented the email verify and reset password

--- client/src/pages/login/EmailVerify.jsx
+++ client/src/pages/login/EmailVerify.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import axios from 'axios';
+import { AppContext } from '../../context/AppContext'
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import { useEffect } from "react";
+
 
 const EmailVerify = () => {
-    const inputRefs = React.useRef([])
+
+    axios.defaults.withCredentials = true;
+
+    const { backendUrl, isLoggedin, userData, getUserData } = useContext(AppContext);
+
+    const navigate = useNavigate();
+
+    const inputRefs = React.useRef([]);
 
     const handleInput = (e, index) => {
         if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
@@ -13,9 +26,7 @@ const EmailVerify = () => {
             inputRefs.current[index - 1].focus();
         }
     }
-
     const handlePaste = (e) => {
-
         const paste = e.clipboardData.getData('text')
         const pasteArray = paste.split('')
         pasteArray.forEach((char, index) => {
@@ -25,16 +36,39 @@ const EmailVerify = () => {
         })
     }
 
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault();
+            const otpArray = inputRefs.current.map(e => e.value)
+            const otp = otpArray.join('')
+
+            const { data } = await axios.post(backendUrl + '/api/auth/verify-account', { otp })
+
+            if (data.success) {
+                toast.success(data.message)
+                getUserData()
+                navigate('/')
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+    useEffect(() => {
+        isLoggedin && userData && userData.isAccountVerified && navigate('/')
+    }, [isLoggedin, userData])
 
     return (
-        <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-600 to-white-400'>
+        <div onSubmit={onSubmitHandler}
+            className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-600 to-white-400'>
             <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
                 <h1 className="text-white text-2xl font-semibold text-center mb-4">Email Verify OTP
                 </h1>
                 <p className="text-center mb-6 text-indigo-300">
                     Enter the 6-digit code sent to your email id.
                 </p>
-                <div className="flex jutify-between mb-8 gap-2" onPaste={handlePaste}>
+                <div className="flex jutify-between mb-8 gap-1" onPaste={handlePaste}>
                     {Array(6).fill(0).map((_, index) => (
                         <input type="text" maxLength="1" key={index} required
                             className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
